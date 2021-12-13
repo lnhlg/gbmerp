1ac9c626e1a89e5479e30f8764278e95a3487294
lnhlg <jxlnhlg@gmail.com>
2021-12-13 08:45:19 +0800

    Update user.go

--- app/interface/internal/data/user.go
+++ app/interface/internal/data/user.go
@@ -38,18 +38,41 @@ func (us *userRepo) Login(
 		return 0, err
 	}
 
-	json := jsoniter.ConfigCompatibleWithStandardLibrary
-	usJs, err := json.Marshal(&usInfo)
-	if err != nil {
-		return 0, err
-	}
+	////存为json格式
+	//json := jsoniter.ConfigCompatibleWithStandardLibrary
+	//usJs, err := json.Marshal(&usInfo)
+	//if err != nil {
+	//	return 0, err
+	//}
+	//
+	//if err := us.data.rdb.Set(
+	//	ctx,
+	//	key,
+	//	usJs,
+	//	time.Hour*time.Duration(us.expire),
+	//).Err(); err != nil {
+	//	return 0, err
+	//}
 
-	if err := us.data.rdb.Set(
-		ctx,
-		key,
-		usJs,
-		time.Hour*time.Duration(us.expire),
-	).Err(); err != nil {
+	//存为哈希表
+	_, err = us.data.rdb.Pipelined(ctx, func(rdb redis.Pipeliner) error {
+		rdb.HSet(ctx, key, "ID", usInfo.ID)
+		rdb.HSet(ctx, key, "Name", usInfo.Name)
+		rdb.HSet(ctx, key, "UserType", usInfo.UserType)
+		rdb.HSet(ctx, key+":staff", "ID", usInfo.Staff.ID)
+		rdb.HSet(ctx, key+":staff", "No", usInfo.Staff.No)
+		rdb.HSet(ctx, key+":staff", "Name", usInfo.Staff.Name)
+		rdb.HSet(ctx, key+":staff", "Gender", usInfo.Staff.Gender)
+		rdb.HSet(ctx, key+":Staff"+":duty", "ID", usInfo.Staff.Duty.ID)
+		rdb.HSet(ctx, key+":Staff"+":duty", "No", usInfo.Staff.Duty.No)
+		rdb.HSet(ctx, key+":Staff"+":duty", "Name", usInfo.Staff.Duty.Name)
+		rdb.HSet(ctx, key+":Staff"+":duty", "Level", usInfo.Staff.Duty.Level)
+		rdb.HSet(ctx, key+":Staff"+":dept", "Level", usInfo.Staff.Dept.ID)
+		rdb.HSet(ctx, key+":Staff"+":dept", "Level", usInfo.Staff.Dept.No)
+		rdb.HSet(ctx, key+":Staff"+":dept", "Level", usInfo.Staff.Dept.Name)
+		return nil
+	})
+	if err != nil {
 		return 0, err
 	}
 
